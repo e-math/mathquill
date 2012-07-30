742b71f594a1cf059f1aeac50db8a0f71783a594
Petri Salmela <pesasa@iki.fi>
2012-07-30 11:57:58 +0300

    Change canonical name of \< and \> to \lt and \gt to avoid conflicts with html.

--- src/symbols.js
+++ src/symbols.js
@@ -174,8 +174,8 @@ CharCmds['*'] = LatexCmds.sdot = LatexCmds.cdot =
 //semantically should be &sdot;, but &middot; looks better
 
 LatexCmds['='] = bind(BinaryOperator, '=', '=');
-LatexCmds['<'] = bind(BinaryOperator, '<', '&lt;');
-LatexCmds['>'] = bind(BinaryOperator, '>', '&gt;');
+LatexCmds['lt'] = bind(BinaryOperator, '\\lt', '&lt;');
+LatexCmds['gt'] = bind(BinaryOperator, '\\gt', '&gt;');
 
 LatexCmds.notin =
 LatexCmds.sim =
@@ -208,9 +208,9 @@ LatexCmds.prop = LatexCmds.propto = bind(BinaryOperator,'\\propto ','&prop;');
 
 LatexCmds['≈'] = LatexCmds.asymp = LatexCmds.approx = bind(BinaryOperator,'\\approx ','&asymp;');
 
-LatexCmds.lt = bind(BinaryOperator,'<','&lt;');
+LatexCmds['<'] = bind(BinaryOperator,'\\lt','&lt;');
 
-LatexCmds.gt = bind(BinaryOperator,'>','&gt;');
+LatexCmds['>'] = bind(BinaryOperator,'\\gt','&gt;');
 
 LatexCmds['≤'] = LatexCmds.le = LatexCmds.leq = bind(BinaryOperator,'\\le ','&le;');
 
